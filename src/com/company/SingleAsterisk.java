cc72daded3df51430699ad007b38819db360de76
Mrinal Kanti Ghosh <mrinal2196ghosh@gmail.com>
2018-08-03 07:42:15 +0530

    SingleAsterisk class added

---
+++ src/com/company/SingleAsterisk.java
@@ -0,0 +1,9 @@
+package com.company;
+public class SingleAsterisk {
+
+    public static void main(String[] args) {
+        Asterisk asterisk = new Asterisk();
+        System.out.println(asterisk.getAsterisk());
+
+    }
+}
